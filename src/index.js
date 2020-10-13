060e964dedb47f1a4f2091c373e1805cb3c99812
Narendra <narendra.sabale@byjus.com>
2020-10-13 11:30:52 +0530

    assigned default value for  seekTime if not passed

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ class VideoPlayer extends React.PureComponent{
 	}
 
 	componentDidMount() {
-    const {src, srcKey, authToken, seekTime, streamingConfig, abrConfig, drmConfig, uiConfig, onVideoEnd, onPlayFailed, getCurrentSeekTime, removeRightClick, disableControls, disableConsoleControls} = this.props
+    const {src, srcKey, authToken, seekTime=0, streamingConfig, abrConfig, drmConfig, uiConfig, onVideoEnd, onPlayFailed, getCurrentSeekTime, removeRightClick, disableControls, disableConsoleControls} = this.props
     
     const video = this.videoRef.current;
 		const videoContainer = this.videoContainer.current;
